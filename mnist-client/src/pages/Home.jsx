2ff694ae5de4722d7786cf7004688ba8f5ec1ec2
louispaulet <louispaulet13@gmail.com>
2024-09-03 19:19:53 +0200

    add subtitle in italics in the home page

--- mnist-client/src/pages/Home.jsx
+++ mnist-client/src/pages/Home.jsx
@@ -11,7 +11,8 @@ const Home = () => {
   <div className="flex flex-col min-h-screen bg-gray-100">
       <Header />
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">MNIST Digit Recognition</h1>
+      <h1 className="text-4xl font-bold my-4">MNIST Digit Recognition</h1>
+      <h2 className="text-4xl font-bold mb-8 italic">-in the browser-</h2>
       <Canvas setPrediction={setPrediction} />
       <Inference prediction={prediction} />
     </div>
